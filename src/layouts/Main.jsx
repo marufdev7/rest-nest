0c3b1d7948c9d85339929662ea84b0222abd4bb8
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-06 00:23:43 +0600

    login and signup page create and rerendaring problem slove

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
-import Home from '../pages/Home/Home';
+import { Outlet } from 'react-router-dom';
+
 
 const Main = () => {
      return (
           <div>
                <Header></Header>
-               <Home></Home>
+               <main>
+                    <Outlet></Outlet>
+               </main>
                <Footer></Footer>
           </div>
      );
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+     return (
+          <div>
+               <h2>Sing UP page is coming..</h2>
+          </div>
+     );
+};
+
+export default SignUp;
