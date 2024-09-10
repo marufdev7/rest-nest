27b6bb9e041898a04a0bb2fa8130f520ea0ceffa
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-10 23:38:01 +0600

    added progress bar while loading

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,9 +3,15 @@ import { AuthContext } from './AuthProviders';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-     
-     const { user } = useContext(AuthContext);
-     
+
+     const { user, loading } = useContext(AuthContext);
+
+     if (loading) {
+          return <div className="flex justify-center items-center h-screen">
+               <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+          </div>
+     };
+
      if (user) {
           return children;
      }
