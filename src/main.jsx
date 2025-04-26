212eb1f88478cafbd01c87617e05820acc010271
mrfhmd7 <mrf.hmd7@gmail.com>
2025-04-26 23:15:38 +0600

    i react helmet async

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes.jsx'
 import AuthProviders from './routes/AuthProviders.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProviders>
-      <RouterProvider router={router}></RouterProvider>
-    </AuthProviders>
+    <HelmetProvider>
+      <AuthProviders>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProviders>
+    </HelmetProvider>
   </StrictMode>,
 )
